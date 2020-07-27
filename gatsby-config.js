e2c6cbeaeff5d9f13d2b6109ff20417b05e2ef31
Simon Nielsen <simon@minatjanster.se>
2020-07-27 13:10:35 +0200

    Added netlify cms and pwa support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
     title: `My little blog`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `A little blog built on Gatsby's default starter.`,
+    author: `@siMs0n`,
+    siteUrl: "https://suspicious-hopper-9e9e1a.netlify.app",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,20 +26,22 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Level Up Blog`,
+        short_name: `Bloggy`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#50536b`,
+        theme_color: `#50536b`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     "gatsby-transformer-remark",
     "gatsby-plugin-styled-components",
+    "gatsby-plugin-netlify-cms",
     "gatsby-plugin-netlify",
+    "gatsby-plugin-sitemap",
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 };
